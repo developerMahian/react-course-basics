3ab69c8608463c84ff5ba02f0b9e2ecd222ab614
developerMahian <developermahian@gmail.com>
2022-02-20 20:34:43 +0600

    BS:layout fixed

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-
 import Card from "../card/card.component";
 
-import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "./card-list.styles.css";
 
 const CardList = ({ users }) => {
 	return (
-		<div className="card-list row">
-			{users.map((user) => (
-				<Card
-					className="col-lg-3 col-md-4 col-sm-6"
-					user={user}
-					key={user["id"]}
-				/>
-			))}
+		<div className="card-list container">
+			<div className="row">
+				{users.map((user) => (
+					<Card user={user} key={user["id"]} />
+				))}
+			</div>
 		</div>
 	);
 };
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "./card.styles.css";
 
 const Card = ({ user }) => {
 	return (
-		<div className="card-container">
-			<img
-				src={`https://robohash.org/${user["id"]}?set=set2`}
-				alt={user["name"]}
-				width="200"
-			/>
-			<h1>{user["name"]}</h1>
+		<div className="col-lg-3 col-md-4 col-sm-6 py-3">
+			<div className="card-container">
+				<img
+					src={`https://robohash.org/${user["id"]}?set=set2`}
+					alt={user["name"]}
+				/>
+				<h1>{user["name"]}</h1>
+			</div>
 		</div>
 	);
 };
