0f9223ab0b0fd13c049a012236a4c66e9b4d9a22
developerMahian <developermahian@gmail.com>
2022-02-20 20:20:48 +0600

    card list layout updated with bootstrap

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Card from "../card/card.component";
 
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "./card-list.styles.css";
 
 const CardList = ({ users }) => {
 	return (
-		<div className="card-list">
+		<div className="card-list row">
 			{users.map((user) => (
-				<Card user={user} key={user["id"]} />
+				<Card
+					className="col-lg-3 col-md-4 col-sm-6"
+					user={user}
+					key={user["id"]}
+				/>
 			))}
 		</div>
 	);
